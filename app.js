61d74ad7982ac7cf9f80b968fcb8daf344de0056
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-06-21 10:46:45 -0300

    inicio do projeto jogo em javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == numeroSecreto){
+    console.log('Você acertou o número secreto, parabéns! (29)');
+}
