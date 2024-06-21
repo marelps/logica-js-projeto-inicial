cd43e99d78e26a47a94c684b1a9a6cc6e81a4c9c
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-06-21 11:14:49 -0300

    inserção do else

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
 
-if (numeroSecreto == numeroSecreto){
-    console.log('Você acertou o número secreto, parabéns! (29)');
+// se o chute for igual ao número secreto
+if (chute == numeroSecreto){
+    alert('Você acertou o número secreto, parabéns! (5)');
+} else {
+    alert('Você errou! :(');
 }
+
+// o console é uma ferramenta do desenvolvedor e não para o usuário, portanto no if o idel é deixar o 'alert' e não 'console.log'
